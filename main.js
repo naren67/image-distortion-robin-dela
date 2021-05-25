45e00634c93d2c0a4de064acdbf14a7dee49d903
naren67 <naren678n@gmail.com>
2021-05-25 11:04:25 +0530

    robin dela

---
+++ main.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+          parent: document.querySelector('.img'),
+          image1: './image/1.jpg',
+          image2: './image/2.jpg',
+          displacementImage: './displacement/7.jpg',
+                    // to make image perfect size - height/width in float
+          imagesRatio: '0.80'
+})
